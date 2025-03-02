ae25f4d402bd485fd2182daf9ee4228f2266c58f
nomis51 <nomis51@users.noreply.github.com>
2025-03-02 13:48:49 -0500

    Add tests

---
+++ src/Tests/Watson.Tests/Tests/Commands/LogCommandTests.cs
@@ -0,0 +1,51 @@
+﻿using NSubstitute;
+using Watson.Commands;
+using Watson.Core.Helpers;
+using Watson.Core.Repositories;
+using Watson.Core.Repositories.Abstractions;
+using Watson.Helpers;
+using Watson.Models;
+using Watson.Tests.Abstractions;
+
+namespace Watson.Tests.Tests.Commands;
+
+public class LogCommandTests: ConsoleTest, IDisposable
+{
+    #region Members
+
+    private readonly ISettingsRepository _settingsRepository = Substitute.For<ISettingsRepository>();
+    private readonly LogCommand _sut;
+
+    #endregion
+
+    #region Constructors
+
+    public LogCommandTests()
+    {
+        var idHelper = new IdHelper();
+
+        var frameRepository = new FrameRepository(DbContext, idHelper);
+        _sut = new LogCommand(
+            new DependencyResolver(
+                new ProjectRepository(DbContext, idHelper),
+                frameRepository,
+                new TagRepository(DbContext, idHelper),
+                new TimeHelper(),
+                new FrameHelper(frameRepository),
+                _settingsRepository
+            )
+        );
+    }
+
+    public new void Dispose()
+    {
+        base.Dispose();
+        GC.SuppressFinalize(this);
+    }
+
+    #endregion
+
+    #region Tests
+    
+    #endregion
+}
