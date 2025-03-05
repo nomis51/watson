e713684137670a05a417e186302ee82d68c1c3e9
nomis51 <nomis51@users.noreply.github.com>
2025-03-05 18:44:46 -0500

    #67 - FIX: status command not taking lunch into account

--- src/Tests/Watson.Tests/Tests/Commands/StatusCommandTests.cs
+++ src/Tests/Watson.Tests/Tests/Commands/StatusCommandTests.cs
@@ -5,6 +5,7 @@ using Shouldly;
 using Watson.Commands;
 using Watson.Core;
 using Watson.Core.Helpers;
+using Watson.Core.Models.Settings;
 using Watson.Core.Repositories;
 using Watson.Core.Repositories.Abstractions;
 using Watson.Helpers;
@@ -29,6 +30,8 @@ public class StatusCommandTests : ConsoleTest
     public StatusCommandTests()
     {
         var idHelper = new IdHelper();
+        _settingsRepository.GetSettings()
+            .Returns(new Settings());
 
         var frameRepository = new FrameRepository(DbContext, idHelper);
         _sut = new StatusCommand(
@@ -107,6 +110,41 @@ public class StatusCommandTests : ConsoleTest
         var expectedOutput =
             ConsoleHelper.GetSpectreMarkupOutput(
                 $"id: [green]project[/] ([purple]tag[/]) started at [blue]{time:HH:mm}[/] (00h 00m)");
+
+        // Act
+        var result = await _sut.Run(options);
+        var output = ConsoleHelper.GetMockOutput();
+
+        // Assert
+        result.ShouldBe(0);
+        output.ShouldStartWith(expectedOutput);
+    }
+
+    [Fact]
+    public async Task Run_ShouldTakeLunchTimeIntoAccount()
+    {
+        // Arrange
+        _settingsRepository.GetSettings()
+            .Returns(new Settings
+            {
+                WorkTime =
+                {
+                    LunchStartTime = new TimeSpan(DateTime.Now.Hour, 0, 0),
+                    LunchEndTime = new TimeSpan(DateTime.Now.Hour + 1, 0, 0),
+                }
+            });
+        var time = DateTime.Now.AddHours(-1);
+        await DbContext.Connection.ExecuteAsync("INSERT INTO Frames (Id,ProjectId,Time) VALUES ('id','id',@Time)",
+            new { Time = time.Ticks });
+        await DbContext.Connection.ExecuteAsync("INSERT INTO Projects (Id,Name) VALUES ('id','project')");
+        await DbContext.Connection.ExecuteAsync("INSERT INTO Tags (Id,Name) VALUES ('id','tag')");
+        await DbContext.Connection.ExecuteAsync("INSERT INTO Frames_Tags (Id,FrameId,TagId) VALUES ('id','id','id')");
+        var options = new StatusOptions();
+
+        var expectedMinutes = Convert.ToInt32(60 - time.TimeOfDay.Minutes) - 1;
+        var expectedOutput =
+            ConsoleHelper.GetSpectreMarkupOutput(
+                $"id: [green]project[/] ([purple]tag[/]) started at [blue]{time:HH:mm}[/] (00h {expectedMinutes}m)");
 
         // Act
         var result = await _sut.Run(options);
--- src/Watson/Commands/StatusCommand.cs
+++ src/Watson/Commands/StatusCommand.cs
@@ -21,13 +21,28 @@ public class StatusCommand : Command<StatusOptions>
         var frame = await FrameRepository.GetPreviousFrameAsync(DateTime.Now);
         if (frame is null) return 1;
 
+        var settings = await SettingsRepository.GetSettings();
+        var endTime = DateTime.Now;
+        if (DateTime.Now.TimeOfDay >= settings.WorkTime.LunchStartTime &&
+            DateTime.Now.TimeOfDay <= settings.WorkTime.LunchEndTime)
+        {
+            endTime = new DateTime(
+                DateTime.Now.Year,
+                DateTime.Now.Month,
+                DateTime.Now.Day,
+                settings.WorkTime.LunchStartTime.Hours,
+                settings.WorkTime.LunchStartTime.Minutes,
+                settings.WorkTime.LunchStartTime.Seconds
+            );
+        }
+
         AnsiConsole.MarkupLine(
             "{0}: {1} ({2}) started at {3} ({4})",
             frame.Id,
             $"[green]{frame.Project?.Name ?? "-"}[/]",
             $"[purple]{string.Join("[/], [purple]", frame.Tags.Select(e => e.Name))}[/]",
             $"[blue]{TimeHelper.FormatTime(frame.TimeAsDateTime.TimeOfDay)}[/]",
-            TimeHelper.FormatDuration(DateTime.Now - frame.TimeAsDateTime)
+            TimeHelper.FormatDuration(endTime - frame.TimeAsDateTime)
         );
 
         return 0;
