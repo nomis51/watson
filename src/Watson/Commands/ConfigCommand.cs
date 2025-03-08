1390206b9912afc8aae5cde32bac66dddc3a997d
nomis51 <nomis51@users.noreply.github.com>
2025-03-08 10:16:58 -0500

    (wip) add config command

---
+++ src/Watson/Commands/ConfigCommand.cs
@@ -0,0 +1,50 @@
+﻿using Watson.Commands.Abstractions;
+using Watson.Models.Abstractions;
+using Watson.Models.CommandLine;
+
+namespace Watson.Commands;
+
+public class ConfigCommand : Command<ConfigOptions>
+{
+    #region Constructors
+
+    public ConfigCommand(IDependencyResolver dependencyResolver) : base(dependencyResolver)
+    {
+    }
+
+    #endregion
+
+    #region Public methods
+
+    public override async Task<int> Run(ConfigOptions options)
+    {
+        if (string.IsNullOrEmpty(options.Key)) return 1;
+        if (options.Action.Equals("get", StringComparison.OrdinalIgnoreCase))
+        {
+            return await GetConfig(options.Key);
+        }
+
+        if (options.Action.Equals("set", StringComparison.OrdinalIgnoreCase))
+        {
+            return await SetConfig(options.Key, options.Value);
+        }
+
+        return 1;
+    }
+
+    #endregion
+
+    #region Private methods
+
+    private async Task<int> SetConfig(string key, string? value)
+    {
+        return 1;
+    }
+
+    private async Task<int> GetConfig(string key)
+    {
+        return 1;
+    }
+
+    #endregion
+}
