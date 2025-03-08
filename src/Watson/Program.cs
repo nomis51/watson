d327d144529c4896dcd52cf9940c0861cc6aaabc
nomis51 <nomis51@users.noreply.github.com>
2025-03-08 16:33:10 -0500

    Add global exception handling

--- src/Watson/Program.cs
+++ src/Watson/Program.cs
@@ -1,9 +1,22 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Serilog;
 using Watson.Abstractions;
 using Watson.Extensions;
+using Watson.Helpers;
 
-var services = new ServiceCollection();
-return await services.AddAppResources()
-    .BuildServiceProvider()
-    .GetService<ICli>()!
-    .Run(args);
+LoggingHelper.Configure();
+
+try
+{
+    var services = new ServiceCollection();
+    return await services.AddAppResources()
+        .BuildServiceProvider()
+        .GetService<ICli>()!
+        .Run(args);
+}
+catch (Exception e)
+{
+    Log.Fatal(e, "An unhandled exception occurred.");
+}
+
+return 1;
