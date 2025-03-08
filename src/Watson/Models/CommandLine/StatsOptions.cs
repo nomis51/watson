a3708f5ef8a55181a408047bd9bb993433ff836d
nomis51 <nomis51@users.noreply.github.com>
2025-03-08 16:33:18 -0500

    (wip) stats command

---
+++ src/Watson/Models/CommandLine/StatsOptions.cs
@@ -0,0 +1,43 @@
+﻿using CommandLine;
+
+namespace Watson.Models.CommandLine;
+
+[Verb("stats", HelpText = "Display statistics")]
+public class StatsOptions
+{
+    [Value(0, MetaName = "type", Required = true, HelpText = "Type of statistics")]
+    public string Type { get; set; } = null!;
+
+    [Option('f', "from", HelpText = "From time")]
+    public string FromTime { get; set; } = string.Empty;
+
+    [Option('t', "to", HelpText = "To time")]
+    public string ToTime { get; set; } = string.Empty;
+
+    [Option('y', "year", HelpText = "Display frames of the current year")]
+    public bool Year { get; set; }
+
+    [Option('m', "month", HelpText = "Display frames of the current month")]
+    public bool Month { get; set; }
+
+    [Option('w', "week", HelpText = "Display frames of the current week")]
+    public bool Week { get; set; }
+
+    [Option('d', "day", HelpText = "Display frames of the current day")]
+    public bool Day { get; set; } = true;
+
+    [Option('a', "all", HelpText = "Display all frames")]
+    public bool All { get; set; }
+
+    [Option('p', "project", HelpText = "Display frames of the project(s) only")]
+    public string? Projects { get; set; }
+
+    [Option('t', "tag", HelpText = "Display frames of the tag(s) only")]
+    public string? Tags { get; set; }
+
+    [Option('i', "ignore-project", HelpText = "Ignore frames of the project(s)")]
+    public string? IgnoredProjects { get; set; }
+
+    [Option('g', "ignore-tag", HelpText = "Ignore frames of the tag(s)")]
+    public string? IgnoredTags { get; set; }
+}
