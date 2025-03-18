f1cea75d62230208968984194e00a221715f6d1f
nomis51 <nomis51@users.noreply.github.com>
2025-03-17 20:36:35 -0400

    Update memory check script

--- scripts/memory-check.ps1
+++ scripts/memory-check.ps1
@@ -1,1 +1,2 @@
-﻿dotMemory.exe start ./src/Watson/bin/Debug/net8.0/Watson.exe --save-to-dir=./scripts/.dotMemory/snapshots -- $args
+﻿dotnet build -c Release ./src
+dotMemory.exe start ./src/Watson/bin/Release/net8.0/Watson.exe --save-to-dir=./scripts/.dotMemory/snapshots -- $args
