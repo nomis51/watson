4d8574969228274f2e3c931c69081d720b7e6ec3
nomi51 <nomis51@github.users.noreply.com>
2025-07-15 20:27:01 -0400

    update coverage script

--- scripts/coverage.ps1
+++ scripts/coverage.ps1
@@ -1,19 +1,10 @@
-$dotCoverFilters = @(
-    "+:module=Watson*",
-    "-:module=Watson.Tests",
-    "-:class=Watson.Models.CommandLine.*",
-    "-:class=Watson.Models.DependencyResolver",
-    "-:class=Watson.Extensions.ServicesCollectionExtensions",
-    "-:class=Watson.Commands.Command*",
-    "-:class=Watson.Cli",
-    "-:class=*Program",
-    "-:class=Watson.Core.Models.Settings.*"
-)
-$dotnetTestFilters = @(
-    "FullyQualifiedName~Watson.Tests"
-)
+$testProjectFilePath = "./src/Tests/Watson.Tests/Watson.Tests.csproj"
+$outputFilePath = "./src/Tests/Watson.Tests/bin/Debug/net8.0/TestResults/coverage.xml"
+$reportPath = "./src/Tests/Watson.Tests/bin/Debug/net8.0/TestResults/"
+$reportFilePath = "./src/Tests/Watson.Tests/bin/Debug/net8.0/TestResults/index.html"
 
-$joinedDotCoverFilters = $dotCoverFilters -join ";"
-$joinedDotnetTestFilters = $dotnetTestFilters -join ";"
-dotnet dotcover cover-dotnet --ReportType=HTML --Output=./src/Tests/Watson.Tests/.coverage/report.html --HideAutoProperties --Filters="$joinedDotCoverFilters" -- test --no-build --filter "$joinedDotnetTestFilters" ./src/Tests/Watson.Tests
-Invoke-Item ./src/Tests/Watson.Tests/.coverage/report.html
+dotnet run --project $testProjectFilePath --coverage --coverage-output coverage.xml --coverage-output-format cobertura
+
+reportgenerator -reports:$outputFilePath -targetdir:$reportPath -reporttypes:"Html"
+Start-Process $reportFilePath
+
